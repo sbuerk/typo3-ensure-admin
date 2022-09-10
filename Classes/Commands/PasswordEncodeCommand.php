3abcf685620ea62b0a4398cbfbd47c09d7b72e4c
Stefan Bürk <stefan.buerk@impactmedia.de>
2022-09-11 00:20:56 +0200

    [TASK] Implement PasswordEncodeCommand functionality

--- Classes/Commands/PasswordEncodeCommand.php
+++ Classes/Commands/PasswordEncodeCommand.php
@@ -17,18 +17,54 @@ declare(strict_types=1);
 
 namespace SBUERK\EnsureAdmin\Commands;
 
+use SBUERK\EnsureAdmin\Services\AdminPasswordService;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class PasswordEncodeCommand extends Command
 {
+    /**
+     * @var AdminPasswordService
+     */
+    protected $adminPasswordService;
+
+    public function __construct(AdminPasswordService $adminPasswordService)
+    {
+        $this->adminPasswordService = $adminPasswordService;
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
+        $this->addArgument('plain', InputArgument::REQUIRED, 'Plain password to encode to be used for install tool / .env setting');
+        $this->addOption('json', null, InputOption::VALUE_NONE, 'Output json result');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+        $plainPassword = (string)$input->getArgument('plain'); // @phpstan-ignore-line
+        $asJson = (bool)$input->getOption('json');
+        if ($plainPassword === '') {
+            if ($asJson) {
+                $io->writeln((string)\json_encode(['success' => false, 'password' => '', 'message' => 'Plain password cannot be empty.']));
+            } else {
+                $io->error('Plain password cannot be empty.');
+            }
+            return 1;
+        }
+
+        $hashedPassword = $this->adminPasswordService->getHashedPassword($plainPassword);
+        $message = sprintf('Hashed pasword for %s : %s', $plainPassword, $hashedPassword);
+        if ($asJson) {
+            $io->writeln((string)\json_encode(['success' => true, 'password' => $hashedPassword, 'message' => $message]));
+        } else {
+            $io->error($message);
+        }
         return 0;
     }
 }

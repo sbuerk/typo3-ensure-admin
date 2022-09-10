a28c77820f58b6152f11b1347da4d55999d6f0ea
Stefan Bürk <stefan.buerk@impactmedia.de>
2022-09-10 23:20:19 +0200

    [TASK] Add additional exludes to php-cs-fixer configuration

--- Build/php-cs-fixer/php-cs-fixer.php
+++ Build/php-cs-fixer/php-cs-fixer.php
@@ -6,6 +6,7 @@ $config->setFinder(
     (new PhpCsFixer\Finder())
         ->ignoreVCSIgnored(true)
         ->notPath('/^Build\/php-cs-fixer\/php-cs-fixer.php/')
+        ->notPath('/^Build\/phpunit\/(UnitTestsBootstrap|FunctionalTestsBootstrap).php/')
         ->notName('/^ext_emconf.php/')
 );
 return $config;
